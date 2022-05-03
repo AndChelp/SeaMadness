dd1f9baa214cb1b716639e746d6fba0ba858637f
AndChelp <AndChelp@yandex.ru>
2022-05-03 19:23:44 +0500

    minor changes

--- Assets/Scripts/Ship/Float.cs
+++ Assets/Scripts/Ship/Float.cs
@@ -1,33 +1,35 @@
-using System;
 using UnityEngine;
 
-public class Float : MonoBehaviour
+namespace Ship
 {
-    private LowPolyWater.LowPolyWater water;
-    public Rigidbody rb;
-
-    public float maxDepth = 1f;
-    public float displacementForce = 1f;
-    public float floatCount = 1f;
-    public float waterDrag = 1f;
-    public float waterAngularDrag = 1f;
-
-    private void Awake()
+    public class Float : MonoBehaviour
     {
-        water = FindObjectOfType<LowPolyWater.LowPolyWater>();
-    }
+        private LowPolyWater.LowPolyWater _water;
+        public Rigidbody rb;
 
-    private void FixedUpdate()
-    {
-        var position = transform.position;
-        rb.AddForceAtPosition(Physics.gravity / floatCount, position, ForceMode.Acceleration);
-        var waveYAt = water.GetWaveYAt(position);
-        if (!(position.y < waveYAt)) return;
-        var displacement = Mathf.Clamp01((waveYAt - position.y) / maxDepth) * displacementForce;
-        rb.AddForceAtPosition(new Vector3(0f, -1 * Physics.gravity.y * displacement, 0f), position,
-            ForceMode.Acceleration);
-        rb.AddForce(displacement * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
-        rb.AddTorque(displacement * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime,
-            ForceMode.VelocityChange);
+        public float maxDepth = 1f;
+        public float displacementForce = 1f;
+        public float floatCount = 1f;
+        public float waterDrag = 1f;
+        public float waterAngularDrag = 1f;
+
+        private void Awake()
+        {
+            _water = FindObjectOfType<LowPolyWater.LowPolyWater>();
+        }
+
+        private void FixedUpdate()
+        {
+            var position = transform.position;
+            rb.AddForceAtPosition(Physics.gravity / floatCount, position, ForceMode.Acceleration);
+            var waveYAt = _water.GetWaveYAt(position);
+            if (!(position.y < waveYAt)) return;
+            var displacement = Mathf.Clamp01((waveYAt - position.y) / maxDepth) * displacementForce;
+            rb.AddForceAtPosition(new Vector3(0f, -1 * Physics.gravity.y * displacement, 0f), position,
+                ForceMode.Acceleration);
+            rb.AddForce(displacement * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacement * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime,
+                ForceMode.VelocityChange);
+        }
     }
 }
